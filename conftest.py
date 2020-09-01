355651f825f6091651c642e8b38dec601dd9a4be
dilook <bss4work@yandex.ru>
2020-09-01 14:20:59 +0300

    add: parameterization to test add group

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
+import os
+
 import pytest
+from comtypes.client import CreateObject
 
 from fixture.application import Application
 
@@ -8,3 +11,29 @@ def app(request):
     fixture = Application('C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe')
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def load_from_excel(file_name):
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", f"{file_name}.xlsx")
+    return read_excel(file)
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("excel_"):
+            test_data = load_from_excel(fixture[6:])
+            metafunc.parametrize(fixture, test_data)
+
+
+def read_excel(filename):
+    result = []
+    xl = CreateObject("Excel.Application")
+    try:
+        xl.Visible = 0
+        wb = xl.Workbooks.Open(filename, ReadOnly=True)
+        sheet = wb.Sheets[1]
+        for row in sheet.UsedRange.Rows:
+            result.append(row.Value())
+    finally:
+        xl.Quit()
+    return result
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,9 @@
-def test_add_group(app):
+
+
+def test_add_group(app, excel_groups):
+    data_groups = excel_groups
     old_list = app.group.get_group_list()
-    app.group.add_new_group("my group")
+    app.group.add_new_group(data_groups)
     new_list = app.group.get_group_list()
-    old_list.append("my group")
+    old_list.append(data_groups)
     assert sorted(old_list) == sorted(new_list)
